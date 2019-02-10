750421805b298d2271dacb5cdb400e62b9658acc
Tyler Cartier <tylercartier05@gmail.com>
2019-02-10 15:29:27 -0500

    HatchCoverServo

--- src/main/java/org/usfirst/frc5607/Vinny/subsystems/HatchCoverAssembly.java
+++ src/main/java/org/usfirst/frc5607/Vinny/subsystems/HatchCoverAssembly.java
@@ -1,12 +1,15 @@
+//not sure if using spark or servo
 package org.usfirst.frc5607.Vinny.subsystems;
 import org.usfirst.frc5607.Vinny.Robot;
 import org.usfirst.frc5607.Vinny.OI;
-import edu.wpi.first.wpilibj.Spark;
-
+//import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 public class HatchCoverAssembly
 {
     private static OI oi = Robot.oi;
-    private static Spark _spark = new Spark(1);
+    //private static Spark _spark = new Spark(1);
+    private static Servo _Servo = new Servo(1);
     public HatchCoverAssembly()
     {
         
@@ -15,13 +18,29 @@ public class HatchCoverAssembly
     public void rotateForward()
     {
         //Need to put in stop code
-        _spark.set(0.25);
+        //_spark.set(0.25);
+        _Servo.setAngle(180);
     }
     
 
     public void rotateBackwards()
     {
         //Need to put in stop code
-        _spark.set(-0.25);
+        //_spark.set(-0.25);
+        _Servo.setAngle(0);
+    }
+    public void start()
+    {
+        if(oi.getXboxController1().getTriggerAxis(Hand.kRight)>0.20){
+            rotateForward();
+        }
+        else if(oi.getXboxController1().getTriggerAxis(Hand.kLeft)>0.20){
+            rotateBackwards();
+            
+        }
+
     }
 }
+    
+        
+
