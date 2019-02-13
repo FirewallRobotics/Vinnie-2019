94e3cfd47c179b5a081097d64f80f08403822a2f
williamphale <willybilly19@gmail.com>
2019-02-13 18:05:02 -0500

    Added servo control for Y button

---
+++ src/main/java/org/usfirst/frc5607/Vinny/subsystems/Servotest.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc5607.Vinny.subsystems;
+import org.usfirst.frc5607.Vinny.Robot;
+import edu.wpi.first.wpilibj.Servo;
+
+public class Servotest
+{
+    private int counter;
+    Servo srvo;
+
+    public Servotest(){
+        counter = 0;
+        srvo = new Servo(0);
+    }
+    public void start(){
+        if (Robot.oi.getXboxController1().getYButtonPressed()){
+            counter++;
+            if ((counter % 2) == 0){
+                srvo.set(0);
+            }
+            else{
+                srvo.set(1);
+            }
+        }
+
+    }
+}
