255b20444465c7721ed4e39045002dec84306ee9
williamphale <willybilly19@gmail.com>
2019-02-22 17:50:35 -0500

    Adding code from today that proved the SeeSaw Potentiometer works

--- src/main/java/org/usfirst/frc5607/Vinny/subsystems/Seesaw.java
+++ src/main/java/org/usfirst/frc5607/Vinny/subsystems/Seesaw.java
@@ -2,23 +2,31 @@ package org.usfirst.frc5607.Vinny.subsystems;
 import org.usfirst.frc5607.Vinny.Robot;
 import org.usfirst.frc5607.Vinny.OI;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.GenericHID;
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.SensorCollection;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Seesaw
 {
     private static OI oi = Robot.oi;
 	WPI_TalonSRX _talon = new WPI_TalonSRX(8);
 	private static DoubleSolenoid secondSolenoid = Robot.secondSolenoid;
+	private byte seesawState = 0;
+	private SensorCollection pot = _talon.getSensorCollection();
 	public Seesaw()
-    {/* Config the sensor used for Primary PID and sensor direction */
-        _talon.configSelectedFeedbackSensor(FeedbackDevice.Analog, 
-                                            0,
-				                            30);
+    {
+		/* Config the sensor used for Primary PID and sensor direction */
+        //_talon.configSelectedFeedbackSensor(FeedbackDevice.Analog, 
+         //                                   0,
+			//	                            30);
  
 		/* Ensure sensor is positive when output is positive */
 		_talon.setSensorPhase(true);
+		_talon.setNeutralMode(NeutralMode.Brake);
 
 		/**
 		 * Set based on what direction you want forward/positive to be.
@@ -27,43 +35,76 @@ public class Seesaw
 		_talon.setInverted(false);
 
 		/* Config the peak and nominal outputs, 12V means full */
-		_talon.configNominalOutputForward(0,30);
-		_talon.configNominalOutputReverse(0,30);
-		_talon.configPeakOutputForward(1,30);
-		_talon.configPeakOutputReverse(-1,30);
+		//_talon.configNominalOutputForward(0,30);
+		//_talon.configNominalOutputReverse(0,30);
+		//_talon.configPeakOutputForward(0.20,30);
+		//_talon.configPeakOutputReverse(-0.05,30);
 
 		/**
 		 * Config the allowable closed-loop error, Closed-Loop output will be
 		 * neutral within this range. See Table in Section 17.2.1 for native
 		 * units per rotation.
 		 */
-		_talon.configAllowableClosedloopError(0, 0, 30);
+		//_talon.configAllowableClosedloopError(0, 1, 30);
 
 		/* Config Position Closed Loop gains in slot0, tsypically kF stays zero. */
-		_talon.config_kF(0, 0.0, 30);
-		_talon.config_kP(0, 0.15, 30);
-		_talon.config_kI(0, 0.0, 30);
-		_talon.config_kD(0, 1.0, 30);
+		//_talon.config_kF(0, 0.0, 30);
+		//_talon.config_kP(0, 1, 30);
+		//_talon.config_kI(0, 0.0, 30);
+		//_talon.config_kD(0, 1.0, 30);
 
 		/**
 		 * Grab the 360 degree position of the MagEncoder's absolute
 		 * position, and intitally set the relative sensor to match.
 		 */
-		int absolutePosition = _talon.getSensorCollection().getPulseWidthPosition();
+		//int absolutePosition = 840;
+		//int absolutePosition = _talon.getSensorCollection().
 
 		/* Mask out overflows, keep bottom 12 bits */
-		absolutePosition &= 0xFFF;
-		if (true) { absolutePosition *= -1; }
+		//absolutePosition &= 0xFFF;
+		//if (true) { absolutePosition *= -1; }
 		
 		/* Set the quadrature (relative) sensor to match absolute */
-		_talon.setSelectedSensorPosition(absolutePosition, 0, 30);
-
+		//_talon.setSelectedSensorPosition(absolutePosition, 0, 30);
+		//_talon.set(ControlMode.PercentOutput, 0);
     }
     public void start()
     {
-
+        if (oi.getXboxController1().getBumperReleased(GenericHID.Hand.kRight)) {
+			seesawState++;
+		}
+		if ((seesawState % 3) ==0) {
+			_talon.set(ControlMode.PercentOutput, 0);
+			seesawState = 0;
+		}
+		else if ((seesawState % 3) == 1) { 
+			goToPosition(823);
+		}
+		else if ((seesawState % 3) == 2) { 
+			goToPosition(818);
+		}
+		double analogIn = pot.getAnalogIn();
+		SmartDashboard.putNumber("Potentiometer", analogIn);
+		SmartDashboard.putNumber("SeesawState", seesawState);
+        //double analogIn = ((pot.getAnalogIn() - 7) / 9.07);
+		/*
         if(oi.getXboxController1().getXButtonPressed()){
 			_talon.set(ControlMode.Position, 0.50);
-        }
-    }
+		}*/
+	}
+	
+	public void goToPosition(int pos) {
+		//_talon.setSelectedSensorPosition(840, 0, 30);\
+		SmartDashboard.putNumber("Target Pos", pos);
+		int potvalue = Math.round(pot.getAnalogIn());
+		if (potvalue < pos){
+			_talon.set(ControlMode.PercentOutput, 0.1);
+		}
+		else if (potvalue > pos){
+			_talon.set(ControlMode.PercentOutput, -0.1);
+		}
+		else {
+			_talon.set(ControlMode.PercentOutput, 0);
+		}
+	}
 }
